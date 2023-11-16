ca56e8e526400d43551df85334bb7335b9a23f3d
weishohua <wsh525354@163.com>
2023-11-16 13:56:00 +0800

    更新数据文件

--- com/symon/scripts/fetch_limit_up_stock_jj.py
+++ com/symon/scripts/fetch_limit_up_stock_jj.py
@@ -1,21 +1,22 @@
 """
 东方财富抓取集合竞价数据
 """
-import time
+import datetime
 import pandas as pd
 
 from com.symon.crawler.dfcf import data_api as dfcf
-from com.symon.utils import common_utils
 
 if __name__ == '__main__':
-    limit_up_day = '2023-11-14'
-    limit_up_day_jj = '2023-11-15'
+    # limit_up_day = '2023-11-16'
+    # limit_up_day_jj = '2023-11-17'
+    limit_up_day = str(datetime.date.today() - datetime.timedelta(days=1))
+    limit_up_day_jj = str(datetime.date.today())
 
-    date = common_utils.get_standard_date(timestamp=time.time())
     df = pd.read_csv('../crawler/ths/data/2023-涨停股票.csv', encoding='utf-8')
     filtered_df = df[df['涨停日期'] == limit_up_day]
     step = 0
     all_stock = []
+    total = 0
     for stock_code, stock_name, yesterday_close_price, yesterday_amount in filtered_df[['股票代码', '股票名称', '收盘价', '成交金额']].values:
         stock = {
             '股票代码': stock_code,
@@ -25,6 +26,8 @@ if __name__ == '__main__':
         }
         goal_data = dfcf.get_open_data(limit_day=limit_up_day_jj, stock=stock)
         all_stock.append(goal_data)
+        total = total + 1
     all_stock.sort(key=lambda x: x['竞价涨幅'], reverse=True)
     # dfcf.save_data(all_stock, f'../crawler/ths/data/2023-昨日涨停股票今日竞价数据.csv', True, 'w', 'utf-8')
     dfcf.save_data(all_stock, f'../crawler/ths/data/2023-昨日涨停股票今日竞价数据.csv', False, 'a', 'utf-8')
+    print(f"completed stock total:{total}")
