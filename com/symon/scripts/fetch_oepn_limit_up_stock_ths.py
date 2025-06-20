7657349efafc1b0eab732646868dcc0a8d242503
weishohua <wsh525354@163.com>
2025-06-20 13:55:29 +0800

    更新数据文件

--- com/symon/scripts/fetch_oepn_limit_up_stock_ths.py
+++ com/symon/scripts/fetch_oepn_limit_up_stock_ths.py
@@ -14,9 +14,9 @@ from com.symon.utils import common_utils
 if __name__ == '__main__':
     # limit_up_day = '20231218'
     limit_up_day = common_utils.get_standard_date(time.time(), '%Y%m%d')
-    # open_limit_stock_data = ths.get_open_limit_pool_after_date(ths.get_first_trade_day("20240101"), 400)
+    # open_limit_stock_data = ths.get_open_limit_pool_after_date(ths.get_first_trade_day("20250101"), 400)
     # df = pd.DataFrame(open_limit_stock_data)
-    # df.to_csv(f"../crawler/ths/data/2024-涨停炸板股票.csv", encoding='utf-8', header=True, mode='w', index=False, quoting=csv.QUOTE_NONE)
+    # df.to_csv(f"../crawler/ths/data/2025-涨停炸板股票.csv", encoding='utf-8', header=True, mode='w', index=False, quoting=csv.QUOTE_NONE)
     open_limit_stock_data = ths.get_open_limit_pool_after_date(ths.get_first_trade_day(limit_up_day), 1)
     df = pd.DataFrame(open_limit_stock_data)
-    df.to_csv(f"../crawler/ths/data/2024-涨停炸板股票.csv", encoding='utf-8', header=False, mode='a', index=False,quoting=csv.QUOTE_NONE)
+    df.to_csv(f"../crawler/ths/data/2025-涨停炸板股票.csv", encoding='utf-8', header=False, mode='a', index=False,quoting=csv.QUOTE_NONE)
